5c32f6576c17c816d49e278cdf0f509bfe88099a
mikeku <mikekuforgit@gmail.com>
2020-04-19 18:23:39 +0800

    設定API回傳結果的顯示欄位

--- musics/resources.py
+++ musics/resources.py
@@ -6,6 +6,7 @@ class MusicResource(ModelResource):
     class Meta:
         queryset = Music.objects.all()
         resource_name = 'music'
+        fields = ['name', 'artist']
 
 
 music_resource = MusicResource()
