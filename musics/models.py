0c9a556e78546bdd8c48e4ea0364f9e0041f487a
mikeku <mikekuforgit@gmail.com>
2020-04-19 15:52:58 +0800

    建立musics應用程式的資料模型

--- musics/models.py
+++ musics/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Music(models.Model):
+    name = models.CharField(max_length=50)  # 歌曲名稱
+    artist = models.CharField(max_length=50)  # 歌手
+    album = models.CharField(max_length=50)  # 專輯
+    release_date = models.DateField()  # 發行日期
